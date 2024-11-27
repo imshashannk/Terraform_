c2c4ad38fffb8175da577d311674843d1a407a2e
imshashannk <shashankkhankari0@gmail.com>
2024-11-27 16:24:10 +0530

    Changes

--- Basics/main.tf
+++ Basics/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "this" {
-  ami                     = "0dee22c13ea7a9a67"
+  ami                     = "ami-0dee22c13ea7a9a67"
   instance_type           = "t2.micro"
   subnet_id               = "subnet-07e23f6b841b9feef"
 
