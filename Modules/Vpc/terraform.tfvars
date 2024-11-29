c54e866f4a6f2ac1a3d8665d99aadae0d9689bba
imshashannk <shashankkhankari0@gmail.com>
2024-11-29 17:31:32 +0530

    completly Changes

--- Modules/Vpc/terraform.tfvars
+++ Modules/Vpc/terraform.tfvars
@@ -3,3 +3,5 @@ vpc_cidr_block_value = "10.0.0.0/16"
 cidr_block_az1_value = "10.0.1.0/24"
 
 cidr_block_az2_value = "10.0.2.0/24"
+
+public_subnets_value = ["10.0.101.0/24", "10.0.102.0/24"]
--- Modules/Vpc/variables.tf
+++ Modules/Vpc/variables.tf
@@ -12,3 +12,8 @@ variable "cidr_block_az2_value" {
   description = "CIDR block for the subnet"
   type        = string
 }
+
+variable "public_subnets_value" {
+  description = "A list of public subnets inside the custom VPC"
+  type        = list(string)
+}
