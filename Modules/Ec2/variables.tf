9c4952716af008dec4c643d18cd5a4a38673c464
imshashannk <shashankkhankari0@gmail.com>
2024-11-29 13:43:11 +0530

    completly Changes

--- Modules/Ec2/variables.tf
+++ Modules/Ec2/variables.tf
@@ -1,14 +1,17 @@
 variable "region_value" {
     description = "The region to launch the instance in"
+    type        = string
 }
 
 variable "ami_value" {
-    description = "The AMI ID to use for the instance"  
+    description = "The AMI ID to use for the instance" 
+    type        = string
 }
 
 
 variable "instance_type_value" {
     description = "The instance type to use for the instance"
+    type        = string
 }
 
 
