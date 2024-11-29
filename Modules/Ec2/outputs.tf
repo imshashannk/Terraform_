9e34855b4c1710544182db629eb98be007b5f60a
imshashannk <shashankkhankari0@gmail.com>
2024-11-29 15:37:45 +0530

    completly Changes

---
+++ Modules/Ec2/outputs.tf
@@ -0,0 +1,7 @@
+output "ec2_instance_1_id" {
+  value = aws_instance.my_ec2_1.id
+}
+
+output "ec2_instance_2_id" {
+  value = aws_instance.my_ec2_2.id
+}
--- Modules/Vpc/outputs.tf
+++ Modules/Vpc/outputs.tf
@@ -1,4 +1,7 @@
 output "vpc_id" {
     value = aws_vpc.my_vpc1.id
-  
+} 
+
+output "public_subnets" {
+  value = aws_subnet.public.*.id
 }
