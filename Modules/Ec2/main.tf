df9e0558f83b16d55fe9f3d3e4cb2eeb58093f35
imshashannk <shashankkhankari0@gmail.com>
2024-11-29 18:43:22 +0530

    completly Changes

--- Modules/Ec2/main.tf
+++ Modules/Ec2/main.tf
@@ -2,16 +2,10 @@ provider "aws" {
   region = var.region_value
 }
 
-resource "aws_instance" "my_ec2_1" {
+resource "aws_instance" "my_ec2" {
   ami                     = var.ami_value
   instance_type           = var.instance_type_value
-  subnet_id               = var.subnet_id_value # Reference the first public subnet
+  subnet_id               = var.subnet_id_value  # Reference to the subnet passed from the root module
   associate_public_ip_address = true
-}
 
-resource "aws_instance" "my_ec2_2" {
-  ami                     = var.ami_value
-  instance_type           = var.instance_type_value
-  subnet_id               = var.subnet_id_value  # Reference the second public subnet
-  associate_public_ip_address = true
 }
--- Modules/Ec2/outputs.tf
+++ Modules/Ec2/outputs.tf
@@ -5,3 +5,18 @@ output "ec2_instance_1_id" {
 output "ec2_instance_2_id" {
   value = aws_instance.my_ec2_2.id
 }
+
+output "instance_id" {
+  value       = aws_instance.my_ec2.id
+  description = "The ID of the created EC2 instance"
+}
+
+output "public_ip" {
+  value       = aws_instance.my_ec2.public_ip
+  description = "The public IP of the created EC2 instance"
+}
+
+output "private_ip" {
+  value       = aws_instance.my_ec2.private_ip
+  description = "The private IP of the created EC2 instance"
+}
