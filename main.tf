b57eed4b83b2c2881f901f7c1c0b871046e5934b
imshashannk <shashankkhankari0@gmail.com>
2024-11-29 18:28:10 +0530

    completly Changes

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
 module "vpc" {
-  source = "/home/infivit/nitro/Terraform_/Modules/Vpc"
-
-  vpc_cidr          = var.vpc_cidr_block_value
-  availability_zones = var.azs_value
-  public_subnets     = var.public_subnets_value
+  source              = "/home/infivit/nitro/Terraform_/Modules/Vpc"
+  vpc_cidr            = var.vpc_cidr_block_value
+  availability_zones  = var.azs_value
+  public_subnets      = var.public_subnets_value
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
@@ -14,7 +13,7 @@ module "vpc" {
   }
 }
 
-module "ec2" {
+module "ec2_instance_1" {
   source              = "/home/infivit/nitro/Terraform_/Modules/Ec2"
   region_value        = var.region_value
   instance_type_value = var.instance_type_value
@@ -22,13 +21,10 @@ module "ec2" {
   public_subnets_value = module.vpc.public_subnets  # Use the output from the VPC module
 }
 
-
-module "ec2" {
+module "ec2_instance_2" {
   source              = "/home/infivit/nitro/Terraform_/Modules/Ec2"
   region_value        = var.region_value
   instance_type_value = var.instance_type_value
   ami_value           = var.ami_value
   public_subnets_value = module.vpc.public_subnets  # Use the output from the VPC module
 }
-
-
