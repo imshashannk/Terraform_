96e582a90c1998c2a252e9927217eb49d5b12715
imshashannk <shashankkhankari0@gmail.com>
2024-11-29 17:02:39 +0530

    completly Changes

--- Modules/Ec2/main.tf
+++ Modules/Ec2/main.tf
@@ -5,13 +5,13 @@ provider "aws" {
 resource "aws_instance" "my_ec2_1" {
   ami                     = var.ami_value
   instance_type           = var.instance_type_value
-  subnet_id               = var.vpc.public_subnets[0]  # Reference the first public subnet
+  subnet_id               = var.subnets.public_subnets[0]  # Reference the first public subnet
   associate_public_ip_address = true
 }
 
 resource "aws_instance" "my_ec2_2" {
   ami                     = var.ami_value
   instance_type           = var.instance_type_value
-  subnet_id               = var.vpc.public_subnets_value[1]  # Reference the second public subnet
+  subnet_id               = var.subnets.public_subnets_value[1]  # Reference the second public subnet
   associate_public_ip_address = true
 }
