70706a339c9cb640b3402297d1a3be5f4f61558e
imshashannk <shashankkhankari0@gmail.com>
2024-11-29 18:26:03 +0530

    completly Changes

---
+++ output.tf
@@ -0,0 +1,22 @@
+output "vpc_id" {
+    value = aws_vpc.my_vpc1.id
+} 
+
+output "public_subnets_value" {
+  description = "List of public subnets"
+  value       = aws_subnet.public.*.id  # or however you define the public subnets in your VPC module
+}
+
+
+output "subnet_az1_id" {
+  value = aws_subnet.az1.id
+}
+
+output "subnet_az2_id" {
+  value = aws_subnet.az2.id
+}
+
+output "public_subnets" {
+  value = [aws_subnet.az1.id, aws_subnet.az2.id]
+  description = "List of public subnet IDs"
+}
