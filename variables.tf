e7d6a5eaa3ee6cbc9d2dcd9b06db3c0fe2eec050
imshashannk <shashankkhankari0@gmail.com>
2024-11-29 12:36:37 +0530

    completly Changes

--- variables.tf
+++ variables.tf
@@ -2,37 +2,45 @@
 
 variable "vpc_cidr_block_value" {
   description = "CIDR block for the custom VPC"
+  type        = string
 }
 
 variable "azs_value" {
   description = "A list of availability zones in the region"
+  type        = list(string)
 }
 
 variable "private_subnets_value" {
   description = "A list of private subnets inside the custom VPC"
+  type        = list(string)
 }
 
 variable "public_subnets_value" {
   description = "A list of public subnets inside the custom VPC"
+  type        = list(string)
 }
 
 # Ec2 
 
 variable "instance_type_value" {
   description = "The type of instance to start"
+  type = string
 }
 
 variable "ami_value" {
   description = "The AMI to use for the instance"
+  type = string
   
 }
 
 variable "ec2_region1_value" {
   description = "The region to launch the instance in"
+  type = string
   
 }
 
 variable "ec2_region2_value" {
   description = "The region to launch the instance in"
+  type = string
   
 }
